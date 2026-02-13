5142dd455995f880fecd6a70028df050b3db1a1f
Jennifer <jennifer_str@icloud.com>
2026-02-13 17:35:40 +0100

    Magazine: microscopic SVG (3-4px) and highest (-mt-14)

--- src/pages/MagazinArticle.tsx
+++ src/pages/MagazinArticle.tsx
@@ -340,7 +340,7 @@ const MagazinArticle = ({ lang = "de" }: MagazinArticleProps) => {
                         </div>
                       )}
                     </div>
-                    <div className="flex items-start justify-end max-h-[6px] lg:max-h-[8px] -mt-12">
+                    <div className="flex items-start justify-end max-h-[3px] lg:max-h-[4px] -mt-14">
                       <SwissMap event={event} />
                     </div>
                   </div>
